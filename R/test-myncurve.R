1d07206362144ae3ba799f671f05a2f66036f884
Preston Vitosh <preston.e.vitosh-1@ou.edu>
2024-10-04 16:23:51 -0500

    test myncurve

--- R/test-myncurve.R
+++ R/test-myncurve.R
@@ -4,19 +4,19 @@ test_that("correct list returned", {
   sigma <- 5
   a <- 4
   expect_equal(myncurve(mu, sigma, a),
-               list(mu, sigma, round(pnorm(a, mu, sigma), 4)))
+               list(mu = mu, sigma = sigma, area = round(pnorm(a, mu, sigma), 4)))
 
   # Test 2:
   mu <- 1
   sigma <- 2
   a <- 0
   expect_equal(myncurve(mu, sigma, a),
-               list(mu, sigma, round(pnorm(a, mu, sigma), 4)))
+               list(mu = mu, sigma = sigma, area = round(pnorm(a, mu, sigma), 4)))
 
   # Test 3:
   mu <- -5
   sigma <- 3
   a <- -4
   expect_equal(myncurve(mu, sigma, a),
-               list(mu, sigma, round(pnorm(a, mu, sigma), 4)))
+               list(mu = mu, sigma = sigma, area = round(pnorm(a, mu, sigma), 4)))
 })
