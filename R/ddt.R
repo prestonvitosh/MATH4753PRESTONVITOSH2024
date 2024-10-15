c473854b3be0cbe181f3cb234bd65c3f45e2f3f6
Preston Vitosh <preston.e.vitosh-1@ou.edu>
2024-10-15 03:49:48 -0500

    fix ddt documentation

--- R/ddt.R
+++ R/ddt.R
@@ -4,9 +4,12 @@
 #'
 #' @format A data frame with 144 rows and 5 variables:
 #' \describe{
+#'   \item{RIVER}{location of the fish when caught}
+#'   \item{MILE}{location within river when caught}
+#'   \item{SPECIES}{species of the fish}
 #'   \item{LENGTH}{length in cm}
 #'   \item{WEIGHT}{weight of the fish, in gms}
-#'   ...
+#'   \item{DDT}{DDT concentration found, in ppm}
 #' }
 #' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
 "ddt"
