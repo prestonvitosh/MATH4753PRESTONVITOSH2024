78beb0d72b4cbaf5d20ffd2942ea4c582046d89e
Preston Vitosh <preston.e.vitosh-1@ou.edu>
2024-10-15 04:07:05 -0500

    fix function name in test file

--- tests/testthat/test-mynbin.R
+++ tests/testthat/test-mynbin.R
@@ -3,11 +3,11 @@ test_that("correct probability returned", {
   y <- 10
   r <- 3
   p <- 0.4
-  expect_equal(mybin(y, r, p), choose(y-1,r-1)*p^r*(1-p)^(y-r))
+  expect_equal(mynbin(y, r, p), choose(y-1,r-1)*p^r*(1-p)^(y-r))
 
   # Test 2:
   y <- 5
   r <- 4
   p <- 0.7
-  expect_equal(mybin(y, r, p), choose(y-1,r-1)*p^r*(1-p)^(y-r))
+  expect_equal(mynbin(y, r, p), choose(y-1,r-1)*p^r*(1-p)^(y-r))
 })
